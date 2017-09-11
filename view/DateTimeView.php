1cbda976a918153bc59a60ee6dd5b8a2048f9d60
William Robertson <wr222ae@student.lnu.se>
2017-09-11 18:26:59 +0200

    testing if test goes through

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,8 +5,12 @@ class DateTimeView {
 
 	public function show() {
 
-    $timeString = getdate();
+    $timeArray = getdate();
+    $hoursString = localtime(time(), true);
 
-		return '<p>' . $timeString["weekday"] . ', the ' . $timeString["mday"] . 'th of ' . $timeString["month"] . ' ' . $timeString["year"] . ', The time is'.  '</p>';
+    var_dump($hoursString);
+    $timeString = $timeArray['weekday'] . ', the ' . $timeArray['mday'] . 'th of ' . $timeArray['month'] . ' ' . $timeArray['year'] . ', The time is '. $hoursString['tm_hour'] . ':' . $hoursString['tm_min'] . ':' . $hoursString['tm_sec'];
+
+		return '<p>' . $timeString . '</p>';
 	}
 }
