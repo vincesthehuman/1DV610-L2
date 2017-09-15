f0a9de9799b33782b23be51b41633e69d4151a12
William Robertson <wr222ae@student.lnu.se>
2017-09-15 16:48:07 +0200

    testing out the code

--- view/LoginView.php
+++ view/LoginView.php
@@ -8,7 +8,9 @@ class LoginView {
 	private static $cookieName = 'LoginView::CookieName';
 	private static $cookiePassword = 'LoginView::CookiePassword';
 	private static $keep = 'LoginView::KeepMeLoggedIn';
-	private static $messageId = 'LoginView::Message';
+  private static $messageId = 'LoginView::Message';
+  
+
 
 
 	/**
@@ -19,7 +21,7 @@ class LoginView {
 	 * @return  void BUT writes to standard output and cookies!
 	 */
 	public function response() {
-    $message = '';
+    $message = 'Username is missing';
 		
 		$response = $this->generateLoginFormHTML($message);
 		//$response .= $this->generateLogoutButtonHTML($message);
