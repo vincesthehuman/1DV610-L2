9fa56e1926fa23cab35197a9a1fe4c7e2ca12a5d
wiRoboto <c.william.robertson@gmail.com>
2017-09-21 14:44:02 +0200

    pushing from newly installed machine

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -3,7 +3,7 @@
 class DateTimeView {
 
 	public function show() {
-    //Returns a date with following format: Wednesday, the 20th of September 2017, The time is 17:13:29
+    //Returns a date with following format "Wednesday, the 20th of September 2017, The time is 17:13:29"
 		return '<p>' . date('l\, \t\h\e\ jS \o\f F Y, \T\h\e \t\i\m\e \i\s G:i:s') . '</p>';
 	}
 }
