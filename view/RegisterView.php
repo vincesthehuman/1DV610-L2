874bf8666a38e6ad047122e3968496efecdddeea
william <c.william.robertson@gmail.com>
2017-09-28 15:31:26 +0200

    edited form so it looks like it should

--- view/RegisterView.php
+++ view/RegisterView.php
@@ -33,23 +33,23 @@ class RegisterView {
 	private function generateRegisterFormHTML() {
 		return '
 		<h2>Register new user</h2>
-		<form method="post">
+		<form action="?register" method="post" enctype="multipart/form-data">
 			<fieldset>
 			<legend>Register a new user - Write username and password</legend>
-				<p id="Message"></p>
-				<label for="UserName" >Username :</label>
-				<input type="text" size="20" name="RegisterView::UserName" id="RegisterView::UserName" value="" />
-				<br/>
-				<label for="RegisterView::Password" >Password  :</label>
-				<input type="password" size="20" name="RegisterView::Password" id="RegisterView::Password" value="" />
-				<br/>
-				<label for="RegisterView::PasswordRepeat" >Repeat password  :</label>
-				<input type="password" size="20" name="RegisterView::PasswordRepeat" id="RegisterView::PasswordRepeat" value="" />
-				<br/>
-				<input id="submit" type="submit" name="DoRegistration"  value="Register" />
-				<br/>
+				<p id="RegisterView::Message"></p>
+				<label for="RegisterView::UserName">Username :</label>
+				<input type="text" size="20" name="RegisterView::UserName" id="RegisterView::UserName" value="">
+				<br>
+				<label for="RegisterView::Password">Password  :</label>
+				<input type="password" size="20" name="RegisterView::Password" id="RegisterView::Password" value="">
+				<br>
+				<label for="RegisterView::PasswordRepeat">Repeat password  :</label>
+				<input type="password" size="20" name="RegisterView::PasswordRepeat" id="RegisterView::PasswordRepeat" value="">
+				<br>
+				<input id="submit" type="submit" name="DoRegistration" value="Register">
+				<br>
 			</fieldset>
-			</form>
+		</form>
 		';
 	}
 	
