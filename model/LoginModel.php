9f0ca99a9e8c7f001d89493d042ee94be7352045
william <c.william.robertson@gmail.com>
2017-10-27 14:54:01 +0200

    save

--- model/LoginModel.php
+++ model/LoginModel.php
@@ -1,15 +1,25 @@
 <?php
 
 class LoginModel{
+  private $notCorrectInputMessage;
 
   public function __construct(){
   }
 
+  //Should call on not emptyCredentials (Checking if password or username is present)
+  //Should call on cehckCredentials if notEmptyCredentials is true
   public function checkLoginCredentials($array){
-    $notCorrectInputMessage = '';
+    if($this->emptyCredentials($array)){
+      return $this->notCorrectInputMessage;
+    }else{
+     return 'Wrong name or password'; 
+    }
+  }
+
+  private function emptyCredentials($array){
     foreach ($array as $key => $value) {
       if (strlen($value) <= 0) {
-        return $notCorrectInputMessage .= ucfirst($key) . ' is missing';
+        return $this->notCorrectInputMessage .= ucfirst($key) . ' is missing';
       }
     }
   }
