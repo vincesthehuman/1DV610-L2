40c72b8c9fcd61990051b35c1c2784bc92c837e0
william <c.william.robertson@gmail.com>
2017-11-07 09:59:14 +0100

    gf

--- model/RegisterUserModel.php
+++ model/RegisterUserModel.php
@@ -24,13 +24,13 @@ class RegisterUserModel{
 
   private function checkNameLength($array){
     if(strlen($array[self::$name]) < self::$minimumNameLength){
-      $this->notCorrectInputMessage .= 'Username has too few characters, at least ' . self::$minimumNameLength . ' characters.';
+      return $this->notCorrectInputMessage .= 'Username has too few characters, at least ' . self::$minimumNameLength . ' characters.';
     }
   }
 
   private function checkPasswordLength($array){
     if (strlen($array[self::$password]) < self::$minimumPasswordLength) {
-      $this->notCorrectInputMessage .= 'Password has too few characters, at least ' . self::$minimumPasswordLength . ' characters.';
+      return $this->notCorrectInputMessage .= 'Password has too few characters, at least ' . self::$minimumPasswordLength . ' characters.';
     }
   }
 
